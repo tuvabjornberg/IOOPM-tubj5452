e9b959d3c31b2d9738093f9e15b62de35e8a1ee6
Tuva Björnberg <tuva.bjornberg.5452@student.uu.se>
2023-09-01 17:53:01 +0200

    lab4 6.5 started, continue on magick

--- labbar/lab4/db.c
+++ labbar/lab4/db.c
@@ -1,6 +1,9 @@
 # include <stdio.h>
 # include <stdbool.h>
+# include <stdlib.h>
 # include "utils.h"
+# include <string.h>
+# include <ctype.h>
 
 typedef struct item {
     char *name; 
@@ -18,9 +21,24 @@ item_t make_item(char *name, char *description, int price, char *shelf) {
     return (item_t) {.name = name, .description = description, .price = price, .shelf = shelf}; 
 }
 
-char *ask_question_shelf(char *question)
-{
-    return ask_question(question, not_empty, (convert_func) strdup).string_value;
+bool check_shelf(char* shelf) {
+    if(isalpha(shelf[0]) && is_number(++shelf) && strlen(shelf) >= 2) {
+        return true; 
+    }
+    else {
+        return false;
+    }
+}
+
+char* ask_question_shelf(char* question) {
+    char* shelf;
+
+    do {
+        shelf = ask_question_string(question);
+    }
+    while (!check_shelf(shelf)); 
+
+    return shelf;
 }
 
 item_t input_item(void) {
@@ -29,9 +47,67 @@ item_t input_item(void) {
     int price = ask_question_int("Skriv in varans pris i ören: "); 
     char *shelf = ask_question_shelf("Skriv in hyllan: "); 
 
+    return (item_t) {.name = name, .description = description, .price = price, .shelf = shelf}; 
 }
 
-int main(void) {
-    item_t ex = { .name = "Hammare", .description = "För att slå", .price = 40, .shelf = "A25"}; 
-    print_item(&ex); 
+char magick(char *array1, char *array2, char *array3) {
+    char buf[255]; 
+}
+
+int main(int argc, char *argv[]) {
+
+    //item_t ex = { .name = "Hammare", .description = "För att slå", .price = 40, .shelf = "A25"}; 
+    //print_item(&ex); 
+    //input_item(); 
+
+    char *array1[] = { "Laser", "Polka", "Extra" }; // TODO: Lägg till!
+    char *array2[] = { "Förnicklad", "Smakande", "Ordinär" }; // TODO: Lägg till!
+    char *array3[] = { "Skruvdragare", "Kola", "Uppgift" }; // TODO: Lägg till!
+    //char *array4[] = { "Småspik", "Hammare", "Tejp"}; 
+    //char *array5[] = { "Tumstock", "Träplanka", "Kruka"}; 
+
+    if (argc < 2) {
+        printf("Usage: %s number\n", argv[0]);
+    }
+    else {
+        item_t db[16]; // Array med plats för 16 varor
+        int db_siz = 0; // Antalet varor i arrayen just nu
+
+        int items = atoi(argv[1]); // Antalet varor som skall skapas
+
+        if (items > 0 && items <= 16) {
+            for (int i = 0; i < items; ++i)
+        {
+        // Läs in en vara, lägg till den i arrayen, öka storleksräknaren
+        item_t item = input_item();
+        db[db_siz] = item;
+        ++db_siz;
+        }
+    }
+    else {
+        puts("Sorry, must have [1-16] items in database.");
+        return 1; // Avslutar programmet!
+    }
+
+    for (int i = db_siz; i < 16; ++i) {
+        char *name = magick(array1, array2, array3); // TODO: Lägg till storlek
+        char *desc = magick(array1, array2, array3); // TODO: Lägg till storlek
+        int price = rand() % 200000;
+        char shelf[] = { rand() % ('Z'-'A') + 'A',
+                         rand() % 10 + '0',
+                         rand() % 10 + '0',
+                         '\0' };
+        item_t item = make_item(name, desc, price, shelf);
+
+        db[db_siz] = item;
+        ++db_siz;
+    }
+
+     // Skriv ut innehållet
+    for (int i = 0; i < db_siz; ++i)
+    {
+        print_item(&db[i]);
+    }
+    }
+    return 0;
 }
