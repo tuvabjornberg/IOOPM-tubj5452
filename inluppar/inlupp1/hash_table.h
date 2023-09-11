d5144bb1500e592c16dfef758574bdd30b112ad3
tubj5452 <tuva.bjornberg.5452@student.uu.se>
2023-09-11 14:46:33 +0200

    start of assignment 1

---
+++ inluppar/inlupp1/hash_table.h
@@ -0,0 +1,46 @@
+#ifndef __HASH_TABLE_H_
+#define __HASH_TABLE_H_
+#pragma once
+
+/**
+ * @file hash_table.h
+ * @author Tuva Björnberg & Gustav Fridén
+ * @date 11/09-2023
+ * @brief Simple hash table that maps integer keys to string values.
+ *
+ * Here typically goes a more extensive explanation of what the header
+ * defines. Doxygens tags are words preceeded by either a backslash @\
+ * or by an at symbol @@.
+ *
+ * @see $CANVAS_OBJECT_REFERENCE$/assignments/gb54499f3b7b264e3af3b68c756090f52
+ */
+
+typedef struct hash_table ioopm_hash_table_t;
+
+/// @brief Create a new hash table
+/// @return A new empty hash table
+ioopm_hash_table_t *ioopm_hash_table_create(void);
+
+/// @brief Delete a hash table and free its memory
+/// @param ht a hash table to be deleted
+void ioopm_hash_table_destroy(ioopm_hash_table_t *ht);
+
+/// @brief add key => value entry in hash table ht
+/// @param ht hash table operated upon
+/// @param key key to insert
+/// @param value value to insert
+void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value);
+
+/// @brief lookup value for key in hash table ht
+/// @param ht hash table operated upon
+/// @param key key to lookup
+/// @return the value mapped to by key (FIXME: incomplete)
+char *ioopm_hash_table_lookup(ioopm_hash_table_t *ht, int key);
+
+/// @brief remove any mapping from key to a value
+/// @param ht hash table operated upon
+/// @param key key to remove
+/// @return the value mapped to by key (FIXME: incomplete)
+char *ioopm_hash_table_remove(ioopm_hash_table_t *ht, int key);
+
+#endif
---
+++ inluppar/inlupp1/hash_table_tests.c
@@ -0,0 +1,63 @@
+#include <CUnit/Basic.h>
+#include "hash_table.h"
+
+int init_suite(void) {
+    // Change this function if you want to do something *before* you
+    // run a test suite
+    return 0;
+}
+
+int clean_suite(void) {
+    // Change this function if you want to do something *after* you
+    // run a test suite
+    return 0;
+}
+
+// These are example test functions. You should replace them with
+// functions of your own.
+void test_create_destroy()
+{
+   ioopm_hash_table_t *ht = ioopm_hash_table_create();
+   CU_ASSERT_PTR_NOT_NULL(ht);
+   ioopm_hash_table_destroy(ht);
+}
+
+int main() {
+    // First we try to set up CUnit, and exit if we fail
+    if (CU_initialize_registry() != CUE_SUCCESS)
+        return CU_get_error();
+
+    // We then create an empty test suite and specify the name and
+    // the init and cleanup functions
+    CU_pSuite my_test_suite = CU_add_suite("My awesome test suite", init_suite, clean_suite);
+    if (my_test_suite == NULL) {
+        // If the test suite could not be added, tear down CUnit and exit
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // This is where we add the test functions to our test suite.
+    // For each call to CU_add_test we specify the test suite, the
+    // name or description of the test, and the function that runs
+    // the test in question. If you want to add another test, just
+    // copy a line below and change the information
+    if (
+        (CU_add_test(my_test_suite, "A simple is_number test", test_create_destroy) == NULL)
+    )
+        {
+        // If adding any of the tests fails, we tear down CUnit and exit
+        CU_cleanup_registry();
+        return CU_get_error();
+      }
+
+    // Set the running mode. Use CU_BRM_VERBOSE for maximum output.
+    // Use CU_BRM_NORMAL to only print errors and a summary
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+
+    // This is where the tests are actually run!
+    CU_basic_run_tests();
+
+    // Tear down CUnit before exiting
+    CU_cleanup_registry();
+    return CU_get_error();
+} 
