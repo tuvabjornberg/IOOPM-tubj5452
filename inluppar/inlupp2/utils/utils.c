b58dc9a15ed643920493fd1a192238dc6ac1969a
NosrednasSucram <marcussanderson02@gmail.com>
2023-10-04 17:24:28 +0200

    added utils header and src

--- inluppar/inlupp2/utils/utils.c
+++ inluppar/inlupp2/utils/utils.c
@@ -1,1 +1,75 @@
-//From Marcus
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+bool not_empty(char *str)
+{
+  return strlen(str) > 0;
+}
+
+bool is_number(char *str)
+{
+  int length = strlen(str);
+  if (!(isdigit(str[0]) || str[0] == '-'))
+    {
+     return false; 
+    }
+  for (int i = 1; i < length; i++)
+  {
+    if (!isdigit(str[i])) return false;
+  }
+  return true;
+}
+
+int read_string(char *buf, int buf_size)
+{
+  if (!fgets(buf, buf_size, stdin))
+    {
+      return 0;
+    }
+
+  if (buf[strlen(buf) - 1] != '\n')
+    {
+      int c;
+      while ((c = getchar()) != '\n' && c != EOF);
+    }
+
+  buf[strcspn(buf, "\n")] = '\0';
+  
+  return strlen(buf);
+}
+
+answer_t ask_question(char *question, check_func check, convert_func convert)
+{
+  char *answer = malloc(BUF_SIZE);
+  
+  puts(question);
+  read_string(answer, BUF_SIZE);      
+
+  while(!check(answer))
+    {
+      puts("Invalid input");
+      read_string(answer, BUF_SIZE);
+    }
+
+  if (convert)
+    {
+      answer_t result = convert(answer);
+      free(answer);
+      return result;
+    }
+  return (answer_t){.string_value = answer};
+}
+
+char *ask_question_string(char *question)
+{
+  return ask_question(question, not_empty, NULL).string_value;
+}
+
+int ask_question_int(char *question)
+{
+  return ask_question(question, is_number, (convert_func)atoi).int_value;
+}
--- inluppar/inlupp2/utils/utils.h
+++ inluppar/inlupp2/utils/utils.h
@@ -1,1 +1,20 @@
-//From Marcus
+#pragma once
+
+#include <stdbool.h>
+
+#define BUF_SIZE 255
+
+typedef union {
+  int int_value;
+  float float_value;
+  char *string_value;
+} answer_t;
+typedef bool(*check_func)(char*);
+typedef answer_t(*convert_func)(char*);
+
+bool not_empty(char *str);
+int read_string(char *buf, int buf_size);
+answer_t ask_question(char *question, check_func check, convert_func convert);
+int ask_question_int(char *question);
+char *ask_question_string(char *question);
+bool is_number(char *str);
